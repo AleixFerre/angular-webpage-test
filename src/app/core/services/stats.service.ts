629f646af4cb29669b9e901beae240839f9dcd8b
Aleix Ferré <thecatalahd@gmail.com>
2022-04-02 19:15:40 +0200

    fixes [IDEA] - Commands Page Features #10 Changing webpage internal name fixes stats data endpoint bug Tots is now selected by default

--- src/app/core/services/stats.service.ts
+++ src/app/core/services/stats.service.ts
@@ -1,21 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
-import { environment } from 'src/environments/environment';
 import { Stat } from '../models/stats.model';
 
-const statsLink = 'https://raw.githubusercontent.com/AleixFerre/CataBot/main/docs/Storage/info.json';
-const statsLinkMock = 'assets/mock/info.json'; // MOCK DATA
+const statsLink = 'assets/mock/info.json';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StatsService {
-  private statsLink = environment.production ? statsLink : statsLinkMock;
-
   constructor(private httpClient: HttpClient) {}
 
   getStats(): Observable<Stat[]> {
-    return this.httpClient.get<Stat[]>(this.statsLink);
+    return this.httpClient.get<Stat[]>(statsLink);
   }
 }
