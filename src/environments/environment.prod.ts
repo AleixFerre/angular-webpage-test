68494900c2c974d679d4c458805d18fc5c50f6b0
Aleix Ferré <thecatalahd@gmail.com>
2022-04-23 02:38:41 +0200

    getting stats from magic api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
+const magicAPILink = 'https://magic-api-9vg26.ondigitalocean.app/api/catabot';
+
 export const environment = {
   production: true,
   root: '/catabot-v2-dev',
+  api: {
+    stats: magicAPILink + '/stats',
+    commands: magicAPILink + '/commands',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,13 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+const magicAPILink = 'http://localhost:3000/api/catabot';
 
 export const environment = {
   production: false,
   root: '',
+  api: {
+    stats: magicAPILink + '/stats',
+    commands: magicAPILink + '/commands',
+  },
 };
-
